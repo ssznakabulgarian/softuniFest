0ed95c24f8663c2605d53643a075e6c3d9979cdd
dtsvetkov13@outlook.com <dtsvetkov13@outlook.com>
2021-01-17 12:25:15 +0200

    Logs controller added

---
+++ WebMonitoringApi/Common/LogResult.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using WebMonitoringApi.Data.Models;
+
+namespace WebMonitoringApi.Common
+{
+    public class LogResult
+    {
+        public int Id { get; set; }
+
+        public HttpStatusCode StatusCode { get; set; }
+
+        public DateTime Sent { get; set; }
+
+        public DateTime Received { get; set; }
+
+        public int ExecutionTime { get; set; }
+
+        public string Url { get; set; }
+    }
+}
--- WebMonitoringApi/Controllers/LogsController.cs
+++ WebMonitoringApi/Controllers/LogsController.cs
@@ -1,34 +1,81 @@
 ﻿using System;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebMonitoringApi.Data;
+using WebMonitoringApi.InputModels;
+using System.Linq;
+using WebMonitoringApi.Common;
+using System.Security.Claims;
+using WebMonitoringApi.Data.Models;
 
 namespace WebMonitoringApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class LogsController : ControllerBase
     {
-        [HttpPost]
-        public IActionResult Post()
+        public readonly ApplicationDbContext _dbContext;
+
+        public LogsController(ApplicationDbContext dbContext)
         {
-            throw new NotImplementedException();
+            _dbContext = dbContext;
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public IActionResult Get(LogInputModel input)
         {
-            throw new NotImplementedException();
-        }
+            if (input != null)
+            {
+                if (input.Url != null && input.Url != string.Empty)
+                {
+                    var logs = _dbContext.Logs.Where(log => log.Url.Value == input.Url);
 
-        [HttpPut]
-        public IActionResult Put()
-        {
-            throw new NotImplementedException();
+                    if (input.SortBy == "dsc")
+                    {
+                        logs.OrderByDescending(log => log.ReceivedOn);
+                    }
+                    else if (input.SortBy == "asc")
+                    {
+                        logs.OrderBy(log => log.ReceivedOn);
+                    }
+
+                    if (input.Limit != 0)
+                    {
+                        logs.Take(input.Limit);
+                    }
+
+                    logs.Select(log => new LogResult
+                    {
+                        Id = log.Id,
+                        Sent = log.SentOn,
+                        Received = log.ReceivedOn,
+                        ExecutionTime = log.SentOn.Subtract(log.ReceivedOn).Milliseconds,
+                        StatusCode = log.StatusCode,
+                        Url = log.Url.Value,
+                    });
+
+                    return Ok(logs);
+                }
+                else
+                {
+                    //user, feature
+                }
+            }
+            return BadRequest();
         }
 
         [HttpDelete]
-        public IActionResult Delete()
+        public IActionResult Delete(LogInputModel input)
         {
-            throw new NotImplementedException();
+            if(input != null)
+            {
+                _dbContext.Logs.Remove(_dbContext.Logs.FirstOrDefault(log => log.Id == input.Id));
+                _dbContext.SaveChanges();
+
+                return Ok();
+            }
+            return BadRequest();
         }
     }
 }
